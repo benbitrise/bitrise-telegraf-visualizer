17f66cee9f82549445e8c870329cb9087fdd41a8
Ben Boral <ben.boral@bitrise.io>
2024-09-27 16:43:02 -0500

    html dir

--- step.sh
+++ step.sh
@@ -5,8 +5,10 @@ pip install pandas matplotlib mpld3 plotly
 
 python visualize.py $telegraf_file
 
-cp resource_usage_chart.html $BITRISE_DEPLOY_DIR/telegraf_viz/index.html
+mkdir -p $BITRISE_HTML_REPORT_DIR/telegraf_viz/
+
+cp resource_usage_chart.html $BITRISE_HTML_REPORT_DIR/telegraf_viz/index.html
 
 echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
 
-envman add --key VISUALIZATION_HTML_PATH --value ${BITRISE_DEPLOY_DIR/telegraf_viz/index.html}
+envman add --key VISUALIZATION_HTML_PATH --value ${BITRISE_HTML_REPORT_DIR/telegraf_viz/index.html}
